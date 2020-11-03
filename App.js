d649e06361830631f267918e0685398c9782072c
ali-moussavi <moussavi73@gmail.com>
2020-11-03 13:55:48 +0330

    fix

--- App.js
+++ App.js
@@ -13,6 +13,7 @@ const movie = require('./routes/movie');
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
+const db = require('./db');
 const app = express();
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -29,7 +30,50 @@ app.get('/register', (req, res) => {
 });
 
 app.post('/signin', (req, res) => {
-	signIn.handleSignIn(req, res, bcrypt);
+	console.log('heloo');
+	const { email, password } = req.body;
+	if (!email || !password) {
+		return res.status(400).json('incorrect form submission');
+	}
+	db
+		.select('*')
+		.from('users')
+		.where('email', '=', email)
+		.then((data) => {
+			let response = { firstname: data[0].firstname, lastname: data[0].lastname };
+			const isValid = bcrypt.compareSync(password, data[0].hash);
+			if (isValid) {
+				const username = data[0].username;
+				db
+					.transaction((trx) => {
+						const sessionNum = Math.round(Math.random() * 10000000).toString();
+						const hashSessionNum = bcrypt.hashSync(sessionNum, saltRounds);
+						return trx
+							.insert({
+								sessionhash: hashSessionNum,
+								userid: data[0].userid
+							})
+							.into('sessions')
+							.returning('userid')
+							.then((userid) => {
+								response.sessionNum = sessionNum;
+								response.username = username;
+								response.userid = userid[0];
+
+								res.json(response);
+							})
+							.then(trx.commit)
+							.catch(trx.rollback);
+					})
+					.catch((err) => res.status(400).json(err));
+			} else {
+				res.status(400).json('wrong credentials');
+			}
+		})
+		.catch((err) => {
+			console.log(err);
+			res.status(400).json('error');
+		});
 });
 
 app.post('/signup', (req, res) => {
